e7adc7d034f38510757a065ae635691b2c720366
dvelajya <dvelajya@mts.ru>
2021-01-11 13:22:34 +0300

    update smoothing

--- statistical_lm/smoothing.py
+++ statistical_lm/smoothing.py
@@ -11,16 +11,19 @@ def no_smoothing(
     """
     Transform word counts to probabilities without smoothing.
 
-    :param counts: mapping from (n - 1) previous words to number of times each word occurred after
+    :param DefaultDict[Tuple[str], CounterType[str]] counts: mapping from (n - 1) previous words to number of times each word occurred after
     :return: mapping from (n - 1) previous words to probability of each word occurred after
     :rtype: DefaultDict[Tuple[str, ...], Dict[str, float]]
     """
 
     probs: DefaultDict[Tuple[str, ...], Dict[str, float]] = defaultdict(dict)
+
     for prefix in tqdm(counts.keys(), desc="transform counts to probabilities"):
         denominator = sum(counts[prefix].values())
+
         for next_word in counts[prefix].keys():
             probs[prefix][next_word] = counts[prefix][next_word] / denominator
+
     return probs
 
 
@@ -32,7 +35,7 @@ def add_k_smoothing(
     Transform word counts to probabilities with add-k (Laplace) smoothing.
     For memory efficiency doesn't save tokens with delta / len(vocab) probability.
 
-    :param counts: mapping from (n - 1) previous words to number of times each word occurred after
+    :param DefaultDict[Tuple[str], CounterType[str]] counts: mapping from (n - 1) previous words to number of times each word occurred after
     :param float delta: add-k (Laplace) smoothing additive parameter
     :return: vocabulary of all words and
         mapping from (n - 1) previous words to probability of each word occurred after
@@ -41,10 +44,13 @@ def add_k_smoothing(
 
     probs: DefaultDict[Tuple[str, ...], Dict[str, float]] = defaultdict(dict)
     vocab = set(word for word_counts in counts.values() for word in word_counts)
+
     for prefix in tqdm(counts.keys(), desc="transform counts to probabilities"):
         denominator = sum(counts[prefix].values()) + delta * len(vocab)
+
         for next_token in counts[prefix].keys():
             probs[prefix][next_token] = (
                 counts[prefix][next_token] + delta
             ) / denominator
+
     return vocab, probs
